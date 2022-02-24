611b4c60f8969757dd435dc3d49e6858b85d137a
Knackie <43431712+Knackie@users.noreply.github.com>
2022-02-24 22:20:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,11 +14,19 @@
 
 using namespace std;
 
+struct Sortie
+{
+    string nameOfProject;
+    vector<string> CollaboUsed;
+
+};
+
 struct Collabo
 {
     string name;
     int nbSkills;
     vector< pair< string, int> > competence;
+    vector< pair<int, int> > used;
 };
 
 struct Project
@@ -28,10 +36,77 @@ struct Project
     int score;
     int nbDaysMax;
     int nbSkills;
+    int tmax;
     vector< pair< string, int> > competence;
+    vector<bool> skillUsed;
+    double prio;
 };
 
+bool isFree(Collabo &a, int debut, int fin)
+{
+    for (int i = 0; a.used.size(); i++)
+    {
+        if(!(debut < a.used[i].first && fin < a.used[i].first || debut > a.used[i].second && fin > a.used[i].second))
+            return false;
+    }
+    return true;
+}
+
+string hasSkill(Collabo& a, vector<pair<string, int> >&comp, vector<bool>& usedSkill)
+{
+    for (int i = 0; i < comp.size(); i++)
+    {
+        for (int j = 0; j < a.competence.size(); j++)
+        {
+            if(a.competence[j].first == comp[i].first && (a.competence[j].second >= comp[i].second))
+               {
+               usedSkill[j] = true;
+               return comp[i].first;
+               }
+        }
+    }
+    return "NO";
+}
+
+
+Sortie listUser(Project& p, vector<Collabo>& allColabo)
+{
+    vector<int> IndiceDesCollabos;
+    Sortie yikes;
+    int min = -1;
+    for (int i = 0; i < p.nbSkills; i++)
+    {
+        for (int j = 0; j < allColabo.size(); j++)
+        {
+            if(hasSkill(allColabo[j],p.competence, p.skillUsed) != "NO" && isFree(allColabo[j], p.tmax, p.nbDaysMax))
+            {
+                IndiceDesCollabos.push_back(j);
+                allColabo[j].name += " ";
+                yikes.CollaboUsed.push_back((allColabo[j]));
+
+                allColabo[j].used.push_back(make_pair(p.tmax, p.nbDaysMax)); // gerer le cas ou un skill n'est pas affecté
+            }
+        }
+    }
+    if (yikes.CollaboUsed.size() == p.nbSkills)
+    {
+        return yikes;
+    }
+    else
+    {
+        for (int i = 0; i < IndiceDesCollabos.size(); i++)
+        {
+            allColabo[IndiceDesCollabos[i]].used.pop_back();
+
+        }
+    }
+
+
+}
+
+
 int main() {
+    bool isGoProject;
     string line;
     ifstream myfile;
     myfile.open("a.in");
@@ -77,36 +152,59 @@ int main() {
                 string comp = line.substr(0,space).c_str();
                 int level =  atoi( line.substr(space).c_str());
                 Contributeur[n - 1].competence.push_back(std::make_pair(comp,level));
+                cout << "name: " <<Contributeur[n-1].name << endl;
+                cout << "line vaut contributeur " << line << endl;
                 if (Contributeur[n-1].competence.size() == Contributeur[n-1].nbSkills)
                 {
                     currentSize = 0;
+                    if (Contributeur.size() == sizeCount)
+                    {
+                        isGoProject = true;
+                        continue;
+                    }
+
+
+
 
                 }
             }
 
-            cout << "sizeCount : " << sizeCount << "sizeProject : " << sizeProject << "currentSize : " << currentSize << endl;
-            if (currentSize == 0 && Contributeur.size() == sizeCount-1 && currentProject == 0 && Projects.size() < sizeProject)
+            //cout << "sizeCount : " << sizeCount << "sizeProject : " << sizeProject << "currentSize : " << currentSize << endl;
+            if (currentProject == 0 && Projects.size() < sizeProject && isGoProject )
             {
                cout << "line vaut " << line << endl;
                int space = line.find(' ');
                Project x;
                x.name = line.substr(0,space).c_str();
-               line = line.substr(space).c_str();
+               line = line.substr(space+1).c_str();
+               cout << "line2 vaut " << line << endl;
                space = line.find(' ');
                x.nbDays = atoi( line.substr(0,space).c_str());
-               line = line.substr(space).c_str();
+               line = line.substr(space+1).c_str();
+               cout << "line3 vaut " << line << endl;
                space = line.find(' ');
                x.score = atoi( line.substr(0,space).c_str());
+               line = line.substr(space+1).c_str();
                space = line.find(' ');
                x.nbDaysMax = atoi( line.substr(0,space).c_str());
                x.nbSkills =atoi( line.substr(space).c_str());
+               x.tmax = x.nbDaysMax - x.nbDays;
+               x.skillUsed.push_back(false);
+               if (x.nbSkills != 0)
+               {
+               x.prio = x.score / (x.nbSkills * x.nbDays);
+               }
+               else
+               {
+                   x.prio = x.score / (x.nbDays);
+               }
                Projects.push_back(x);
                currentProject++;
 
             }
             else if (currentProject!=0)
             {
-
+                cout << "line vaut skills" << line << endl;
                 int n = Projects.size();
                 int space = line.find(' ');
                 string comp = line.substr(0,space).c_str();
@@ -114,7 +212,7 @@ int main() {
                 Projects[n - 1].competence.push_back(std::make_pair(comp,level));
                 if (Projects[n-1].competence.size() == Projects[n-1].nbSkills)
                 {
-                    currentProject =0;
+                    currentProject = 0;
 
                 }
 
@@ -125,8 +223,13 @@ int main() {
 
 
     }
-     cout << sizeCount << " "<< sizeProject << "contributeur name : "<<Contributeur[1].name <<"contributeur skills : "<<Contributeur[0].nbSkills << "skills : " << Contributeur[0].competence[0].first;
-     cout <<  "project name : "<<Projects[0].name;
+
+    for (int i = 0; i <Projects.size(); i++)
+    {
+        cout << Projects[i].name << endl;
+    }
     return 0;
 	/* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
 }
+
+
